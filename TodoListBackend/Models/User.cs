df45ac9f3ef36e1ff653b77f447b678b865fa6eb
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-09-17 15:01:48 -0300

    feat: begin to create migrations

---
+++ TodoListBackend/Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListBackend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(50)]
+    public string Email { get; set; }
+    
+    [Required]
+    public string PasswordHash { get; set; }
+
+    public string RefreshToken { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime UpdatedAt { get; set; }
+}
